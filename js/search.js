4a09a98498c21d06c09e3bd7ed3d3d081ec52e21
Clayton Marriel <claytonlsantana@hotmail.com>
2024-05-17 14:17:35 -0300

    feat: functionalities-updated

--- js/search.js
+++ js/search.js
@@ -1,7 +1,10 @@
+// search.js
 const apiURL = 'https://desafio.xlow.com.br/search';
 const totalProductsElement = document.getElementById('total-products');
 const cardProductsContainer = document.querySelector('.products');
+const toggleLayoutButton = document.getElementById('toggle-layout');
 
+// Fetch products and details
 fetch(apiURL)
   .then(response => {
     if (!response.ok) {
@@ -12,14 +15,11 @@ fetch(apiURL)
   .then(data => {
     console.log('Dados recebidos:', data);
 
-    // Verifica se data é um array e se não está vazio
     if (Array.isArray(data)) {
       const totalProducts = data.length;
       totalProductsElement.textContent = `${totalProducts} produtos`;
 
-      // Itera sobre os produtos e exibe cada um
       data.forEach(produto => {
-        // Faz uma solicitação para obter os detalhes do produto
         fetch(`${apiURL}/${produto.productId}`)
           .then(response => {
             if (!response.ok) {
@@ -28,10 +28,7 @@ fetch(apiURL)
             return response.json();
           })
           .then(detalhesProduto => {
-
-            // Cria um card de produto com os detalhes recebidos
             const cardProduto = criarCardProduto(detalhesProduto);
-            // Adiciona o card de produto ao contêiner de cartões de produtos
             cardProductsContainer.appendChild(cardProduto);
           })
           .catch(error => {
@@ -46,19 +43,12 @@ fetch(apiURL)
     console.error('Erro na solicitação:', error.message);
   });
 
-
 function criarCardProduto(detalhesProduto) {
   const cardProduto = document.createElement('div');
   cardProduto.classList.add('card-products');
-  cardProduto.style.textAlign = 'center';
-
-  const espacamentoEntreElementos = '5px';
 
   const imagemElement = document.createElement('img');
   imagemElement.classList.add('product-image');
-  imagemElement.style.width = '140px';
-  // imagemElement.style.minHeight = 'auto';
-  // imagemElement.style.marginBottom = espacamentoEntreElementos;
 
   const imagemProduct = detalhesProduto[0].items[0].images[0];
   if (imagemProduct && imagemProduct.imageUrl) {
@@ -68,22 +58,26 @@ function criarCardProduto(detalhesProduto) {
   const nomeElement = document.createElement('div');
   nomeElement.classList.add('product-name');
   nomeElement.textContent = detalhesProduto[0].productName;
-  console.log('Nome produto: ', detalhesProduto);
 
   const precoElement = document.createElement('div');
   precoElement.classList.add('product-price');
 
+  const preco = detalhesProduto[0].items[0].sellers[0].commertialOffer.Price;
+  const precoDeLista = detalhesProduto[0].items[0].sellers[0].commertialOffer.ListPrice;
 
-  // Verifica se há vendedores disponíveis e define o preço
-  if (detalhesProduto && detalhesProduto.length > 0 && detalhesProduto[0].items && detalhesProduto[0].items.length > 0 && detalhesProduto[0].items[0].sellers && detalhesProduto[0].items[0].sellers.length > 0) {
-    const preco = detalhesProduto[0].items[0].sellers[0].commertialOffer.Price;
-    if (preco) {
-      precoElement.textContent = `R$ ${preco.toFixed(2)}`; // Formata o preço com duas casas decimais
-    } else {
-      precoElement.textContent = 'Preço indisponível';
-    }
+  if (preco !== undefined) {
+    precoElement.textContent = `R$ ${preco.toFixed(2)}`;
   } else {
     precoElement.textContent = 'Preço indisponível';
+  }
+
+  // Verifica se há um preço de desconto e cria um elemento para ele
+  if (precoDeLista && precoDeLista > preco) {
+    const precoDeDescontoElement = document.createElement('div');
+    precoDeDescontoElement.classList.add('product-discount-price');
+    precoDeDescontoElement.textContent = `De: R$ ${precoDeLista.toFixed(2)}`;
+    precoDeDescontoElement.style.textDecoration = 'line-through';
+    cardProduto.appendChild(precoDeDescontoElement);
   }
 
   const botoesContainer = document.createElement('div');
@@ -93,11 +87,16 @@ function criarCardProduto(detalhesProduto) {
   botaoElement.classList.add('product-button');
   botaoElement.textContent = 'Comprar';
 
-  // Adiciona os elementos ao card do produto
   cardProduto.appendChild(imagemElement);
   cardProduto.appendChild(nomeElement);
   cardProduto.appendChild(precoElement);
-  cardProduto.appendChild(botaoElement);
+  cardProduto.appendChild(botoesContainer);
+  botoesContainer.appendChild(botaoElement);
 
   return cardProduto;
 }
+
+// Toggle layout between 4 and 5 products per line
+toggleLayoutButton.addEventListener('click', () => {
+  cardProductsContainer.classList.toggle('five-per-line');
+});
